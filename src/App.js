2b63696f67663738fbf0e3d40fffb83529df9e8b
luisguillermomolero <gustavoangel745@gmail.com>
2021-11-05 15:43:26 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Slideshow from "./componentes/slideshow";
 //importamos Slideshow y los estilos.css
-import "./estilos.css";
+import './estilos.css';
 import styled from 'styled-components';
 
 
@@ -15,10 +15,10 @@ const App = ( ) => {
 }
 
 const Titulo = styled.p`
-  font.size: 18px;
+  font-size: 20px;
   font_weight: 700;
   text-transform: uppercase;
-  margin -bottom: 10px;
+  margin-bottom: 30px;
 `;
 
 export default App;
--- src/componentes/slideshow.js
+++ src/componentes/slideshow.js
@@ -1,35 +1,128 @@
 import React from "react";
 import img1 from './../img/1.jpeg';
-//import img2 from './../img/2.jpeg';
-//import img3 from './../img/3.jpeg';
-//import img4 from './../img/4.jpeg';
+import img2 from './../img/2.jpeg';
+import img3 from './../img/3.jpeg';
+import img4 from './../img/4.jpeg';
 import {ReactComponent as FlechaIzquierda} from './../img/FlechaIzquierda.svg';
 import {ReactComponent as FlechaDerecha} from './../img/FlechaDerecha.svg';
+import styled from "styled-components";
 
 const Slideshow = () => {
     return (
-        <div>
-            <div>
+        <contenedorPincipal>
+        <ContenedorSlideshow>
+            <Slide>
                 <a href="https://github.com/Gaalo68">
                     <img src={img1} alt=""/>
                     
                 </a>
-                <div>
+                <TextoSlide>
                     <p>15% de descueto</p>
-                </div>
-            </div>
-            <div>
-                <button>
+                </TextoSlide>
+            </Slide>
+            <Slide>
+                <a href="https://github.com/Gaalo68">
+                    <img src={img2} alt=""/>
+                    
+                </a>
+                <TextoSlide>
+                    <p>15% de descueto</p>
+                </TextoSlide>
+            </Slide>
+            <Slide>
+                <a href="https://github.com/Gaalo68">
+                    <img src={img3} alt=""/>
+                    
+                </a>
+                <TextoSlide>
+                    <p>15% de descuento</p>
+                </TextoSlide>
+            </Slide>
+            <Slide>
+                <a href="https://github.com/Gaalo68">
+                    <img src={img4} alt=""/>
+                    
+                </a>
+                <TextoSlide>
+                    <p>15% de descueto</p>
+                </TextoSlide>
+            </Slide>
+            </ContenedorSlideshow>
+            <Controles>
+                <Boton>
                     <FlechaIzquierda/>
-                </button>
-                <button>
+                </Boton>
+                <Boton>
                     <FlechaDerecha/>
-                </button>
-            </div>
-        </div>
+                </Boton>
+            </Controles>
+        </contenedorPincipal>
     );
 }
 
+const contenedorPincipal = styled.div`
+    position: relative;
+
+`;
+
+const ContenedorSlideshow = styled.div`
+    display: flex; /*para q cada imagen queda al lado de otra*/
+    flex-wrap: nowrap;/*para asegurarnos de quede una al lado de otra*/
+`;
+
+const Slide = styled.div`
+    min-width: 100%;
+    overflow: hidden;
+    transitio: .3s ease all;
+    z-index: 10;
+    max-height: 500px;/*para que todas las imagenes quedden del mismo tamaño*/
+    position: relative;
+
+    img {
+        width: 100%;
+        vetical-align: top;
+    }
+`;
+
+const TextoSlide = styled.div`
+    background: rgba(0,0,0, .5);
+    color: #fff;
+    width: 100%;
+    padding: 10px 60px;
+    position: absolute;
+    bottom: 0;
+    text-align: center;
+
+    @media screen and (max-width: 700px) {
+        position: relative;
+        background: #000;
+    }
+`;
+
+const Controles = styled.div`
+    position: absolute;
+    top: 0;
+    z-index: 20;
+    width: 100%;
+    height: 100%;
+    pointer-event: none;
+`;
+
+const Boton = styled.button`
+    pointer-event: all;
+    background: none;
+    border: none;
+    cursor: pointer;
+    outline: none;
+    width: 50%;
+    height: 50%;
+    text-align: center;
+    position: absolute;
+    transition: .3s ease all;
+
+    ${props => props.derecho ? 'rigth: 0' : 'left: 0'};
+`;
+
 export default Slideshow;
 
-//se crea el componente Slideshow y luego se importa ala app.js
+//se crea el componente Slideshow y luego se importa a la app.js
