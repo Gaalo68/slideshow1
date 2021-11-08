4944edd5938f85dabfb2779de3a9c5bbbd132fd2
luisguillermomolero <gustavoangel745@gmail.com>
2021-11-08 08:12:56 -0500

    cuarto commit

--- src/componentes/slideshow.js
+++ src/componentes/slideshow.js
@@ -3,8 +3,8 @@ import img1 from './../img/1.jpeg';
 import img2 from './../img/2.jpeg';
 import img3 from './../img/3.jpeg';
 import img4 from './../img/4.jpeg';
-import {ReactComponent as FlechaIzquierda} from './../img/FlechaIzquierda.svg';
-import {ReactComponent as FlechaDerecha} from './../img/FlechaDerecha.svg';
+import {ReactComponent as FlechaDerecha} from './../img/FlechaIzquierda.svg';
+import {ReactComponent as FlechaIzquierda} from './../img/FlechaDerecha.svg';
 import styled from "styled-components";
 
 const Slideshow = () => {
@@ -49,11 +49,11 @@ const Slideshow = () => {
             </Slide>
             </ContenedorSlideshow>
             <Controles>
-                <Boton>
-                    <FlechaIzquierda/>
+            <Boton derecho>
+                    <FlechaDerecha/>
                 </Boton>
                 <Boton>
-                    <FlechaDerecha/>
+                    <FlechaIzquierda/>
                 </Boton>
             </Controles>
         </contenedorPincipal>
@@ -88,7 +88,7 @@ const TextoSlide = styled.div`
     background: rgba(0,0,0, .5);
     color: #fff;
     width: 100%;
-    padding: 10px 60px;
+    padding: 10px 30px;
     position: absolute;
     bottom: 0;
     text-align: center;
@@ -114,13 +114,23 @@ const Boton = styled.button`
     border: none;
     cursor: pointer;
     outline: none;
-    width: 50%;
-    height: 50%;
+    width: 5%;
+    height: 60%;
     text-align: center;
     position: absolute;
     transition: .3s ease all;
+    &:hover {
+        background: rgba(0,0,0,.2);
+        path {
+            fill: #fff;
+        }
+    }
 
-    ${props => props.derecho ? 'rigth: 0' : 'left: 0'};
+    path {
+        filter: ${props => props.derecho ? 'drop-shadow(-2px 0px 0 px #fff)' : 'drop-shadow(2px 0px 0 px #fff)'}
+    }
+
+    ${props => props.derecho ? 'right: 0' : 'left: 0'}
 `;
 
 export default Slideshow;
